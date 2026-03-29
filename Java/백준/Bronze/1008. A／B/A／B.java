cf18e9fb7a14840961d8cadcfd5200b42b81d610
YooSeokHwan <ysh50227@gmail.com>
2026-03-29 16:20:50 +0900

    [Bronze V] Title: A/B, Time: 188 ms, Memory: 17948 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        Double a, b;
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+        System.out.println(a / b);
+    }
+}
